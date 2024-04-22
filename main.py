582072e0b07d3b4b21275d7bd678a94f8f46fa21
JuanBaez77 <144069853+JuanBaez77@users.noreply.github.com>
2024-04-22 16:41:10 -0300

    actualizamos el menú

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # IMPORT DE CLASES
-from raza import Razas
+from Raza import Razas
 from Persona import Persona
 
 
 def menu():
     print("          MENU\n")
-    n = int(input("Personas // presione(1)\nMascotas // presione(2)\nTratamientos // presione(3)\n-->"))
+    n = int(input("Personas // presione(1)\nMascotas // presione(2)\nTratamientos // presione(3)\n--> "))
 
     if n == 1:
         # Submenú para Personas
         m = int(input(
             "Lista de Clientes // presione(1)\n"
             "Personas Activas // presione(2)\n"
-            "Empleados // presione(3)\n"
+            "Empleados // presione(3)\n--> "
         ))
         
         if m == 1:
@@ -28,7 +28,7 @@ def menu():
             "Mascotas Activas // presione(1)\n"
             "Nueva Mascota // presione(2)\n"
             "Razas de Mascotas // presione(3)\n"
-            "Historial de Vacunas // presione(4)\n-->"
+            "Historial de Vacunas // presione(4)\n--> "
         ))
         
         if m == 1:
@@ -36,19 +36,20 @@ def menu():
         elif m == 2:
             print("Agregar nueva mascota...")
         elif m == 3:
-            j = int(input("Agregar raza // presione(1)\nEliminar raza // presione(2)\n-->"))
+            j = int(input("Agregar raza // presione(1)\nEliminar raza // presione(2)\n--> "))
             if j == 1:
-                raza = Razas(input("Que raza desea agregar -> "))
+                raza = Razas(input("Que raza desea agregar --> "))
                 raza.add_raza()
             elif j == 2:
-                print("Eliminar raza...")
+                eliminar_raza = Razas(input("Que raza desea eliminar --> "))
+                eliminar_raza.delete_raza()
         elif m == 4:
             print("Mostrando historial de vacunas...")
 
     elif n == 3:
         m = int(input(
             "Tratamientos Disponibles // presione(1)\n"
-            "Agenda de Tratamientos // presione(2)\n-->"
+            "Agenda de Tratamientos // presione(2)\n--> "
         ))
 
         if m == 1:
