d0c4cdc853c38f30bf30eedfe3a7c37601bed84b
MartiinVan <vancompernolle26@gmail.com>
2024-04-22 12:59:51 -0300

    Implementa menu avanzado e incorpora contenido en la clase Persona.

--- Persona.py
+++ Persona.py
@@ -1,13 +1,26 @@
+import csv
+
 class Persona:
-    def __init__(self,nombre,apellido,tipoDocumento,documento,estado):
+    def __init__(self,nombre,apellido,tipoDocumento,documento,telefono,estado):
         self.__nombre = nombre
         self.__apellido = apellido
         self.tipoDocumento = tipoDocumento
         self.__documento = documento
+        self.__telefono = telefono
         self.__estado = estado
     
-    def registrarPersona(nombre,apellido,tipoDocumento,documento):
+# METODOS
+    def registrarPersona(nombre,apellido,tipoDocumento,documento,telefono,estado):
         pass
 
-    def eliminarPersona(nombre,persona,estado):
+    def eliminarPersona(nombre,apellido,estado):
         pass
+
+    def mostrarPersona():
+        print("\tLISTA PERSONAS\t")
+        with open("persona.csv", encoding="UTF-8") as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                if int(row[5]) ==1:
+                    print(f"{row[0]} {row[1]}        DOCUMENTO: {row[3]} \t TELEFONO: {row[4]}")
