07e7f3d14c7a14b6e64c6f80869a973bc3ca2cae
AgusC0 <colinaagustin92@gmail.com>
2024-04-28 03:59:58 -0300

    Agregado de Diagnostico y FichaMedica

---
+++ Diagnostico.py
@@ -0,0 +1,5 @@
+class Diagnostico:
+    def __init__(self,Estado,ND,Tipo):
+        self.__NombreDiagnostico = ND
+        self.__Estado = Estado
+        self.__Tipo = Tipo
---
+++ Ficha_Medica.py
@@ -0,0 +1,7 @@
+from Persona import Persona
+from Raza import Razas
+
+class FichaMedica:
+    def __init__(self,Persona,Razas,):
+        self.__Personas = Persona
+        self.__Razas = Razas
