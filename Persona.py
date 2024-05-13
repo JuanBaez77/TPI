84d3407e2ab90c659bed23a82d433d9263e65057
MartiinVan <vancompernolle26@gmail.com>
2024-05-13 12:32:48 -0300

    ARREGLO MENU Y PERSONA

--- Persona.py
+++ Persona.py
@@ -28,7 +28,7 @@ class Persona:
                 tipoPersona = "CLI"
             else:
                 tipoPersona = "EMP"
-            estado = 1
+            estado = True
             nueva_persona = cls(nombre, apellido, tipoDocumento, documento, telefono, tipoPersona, estado)
             with open("persona.csv", "a", newline="") as file:
                 writer = csv.writer(file)
@@ -46,6 +46,18 @@ class Persona:
             print("Error: introduzca un valor válido.")
 
     @classmethod
+    def mostrarPersonaActiva(cls):
+        try:
+            print("\tLISTA PERSONAS\t")
+            with open("persona.csv", encoding="UTF-8") as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    if row[6] == "True":
+                        print(f"{row[0]} {row[1]}        DOCUMENTO: {row[3]} \t TELEFONO: {row[4]}")
+        except FileNotFoundError:
+            print("No se encontró el archivo personas")
+    @classmethod
     def mostrarPersona(cls):
         try:
             print("\tLISTA PERSONAS\t")
@@ -53,7 +65,26 @@ class Persona:
                 reader = csv.reader(file)
                 next(reader)
                 for row in reader:
-                    if int(row[6]) == 1:
-                        print(f"{row[0]} {row[1]}        DOCUMENTO: {row[3]} \t TELEFONO: {row[4]}")
+                    print(f"{row[0]} {row[1]}        DOCUMENTO: {row[3]} \t TELEFONO: {row[4]}")
         except FileNotFoundError:
             print("No se encontró el archivo personas")
+    @classmethod
+    def cambiarEstadoPersona(cls):
+        documento = input("DOCUMENTO DE LA PERSONA QUE DESEA CAMBIAR\n")
+        with open("persona.csv", encoding="UTF-8") as file:
+            reader = csv.reader(file)
+            next(reader)
+            encontrado = 0
+            for row in reader:
+                if row[3] == documento:
+                    print(f"Nombre: {row[0]} {row[2]}")
+                    estado_actual = row[-1]
+                    if estado_actual == "False":
+                        row[-1] = "True"
+                        print("Exito al cambiar estado de Inactivo a Activo")                    
+                    else:
+                        row[-1] = "False"
+                        print("Exito al cambiar estado de Activo a Inactivo")    
+                    encontrado = 1
+            if encontrado == 0:
+                print("Error. Documento no encontrado")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # IMPORT DE CLASES
-from raza import Razas
+from Raza import Razas
 from Persona import Persona
 
 
@@ -11,46 +11,35 @@ def menu():
         if n == 1:
             m = int(input(
                 "Registrar persona // presione(1)\n"
-                "Lista de Clientes // presione(2)\n"
-                "Personas Activas // presione(3)\n"
+                "Personas Activas // presione(2)\n"
+                "Lista de Clientes // presione(3)\n"
                 "Empleados // presione(4)\n"
+                "Ver todas las personas // presione(5)\n"
+                "Volver atras // Presione (6)\n"
             ))
             if m == 1:
                 Persona.registrarPersona()
             elif m == 2:
-                print("Mostrando lista de clientes...")
-            elif m == 3:
                 print("Mostrando personas activas...")
-                Persona.mostrarPersona()
+                Persona.mostrarPersonaActiva()
+            elif m == 3:
+                print("Mostrando lista de clientes...")
             elif m == 4:
                 print("Mostrando empleados...")
-            else:
-                print("Opción inválida. Por favor, intenta de nuevo.")
-
-        elif n == 2:
-            m = int(input(
-                "Mascotas Activas // presione(1)\n"
-                "Nueva Mascota // presione(2)\n"
-                "Razas de Mascotas // presione(3)\n"
-                "Historial de Vacunas // presione(4)\n--> "
-            ))
-        
-            if m == 1: 
-                print("Mostrando mascotas activas...")
-            elif m == 2:
-                print("Agregar nueva mascota...")
-            elif m == 3:
-                j = int(input("Mostrar Razas // presione(1)\nAgregar raza // presione(2)\nEliminar raza // presione(3)\n--> "))
-                if j == 1:
-                    raza = Razas.mostrarRaza()
-            elif j == 2:
-                    raza = Razas(input("Que raza desea agregar --> "))
-                    raza.añadirRaza()
-            elif j == 3:
-                    eliminar_raza = Razas(input("Que raza desea eliminar --> "))
-                    eliminar_raza.eliminarRaza()
-            elif m == 4:
-                print("Mostrando historial de vacunas...")
+            elif m == 5:
+                Persona.mostrarPersona()
+                i = int(input(
+                    "\n\nCambiar estado persona //presione(1)\n"
+                    "volver //presione(2)\n"))
+                if i == 1:
+                    Persona.cambiarEstadoPersona()
+                    continue
+                elif i== 2:
+                    print("Volviendo...")
+                    continue
+            elif m == 6:
+                print("Volviendo...")
+                continue
             else:
                 print("Opción inválida. Por favor, intenta de nuevo.")
 
@@ -62,23 +51,27 @@ def menu():
             "Historial de Vacunas // presione(4)\n--> "
         ))
         
-        if m == 1:
-            print("Mostrando mascotas activas...")
-        elif m == 2:
-            print("Agregar nueva mascota...")
-        elif m == 3:
-            j = int(input("Mostrar Razas // presione(1)\nAgregar raza // presione(2)\nEliminar raza // presione(3)\n--> "))
-            if j == 1:
-                raza = Razas.mostrarRaza()
-            elif j == 2:
-                raza = Razas(input("Que raza desea agregar --> "))
-                raza.añadirRaza()
-            elif j == 3:
-                eliminar_raza = Razas(input("Que raza desea eliminar --> "))
-                eliminar_raza.eliminarRaza()
-        elif m == 4:
-            print("Mostrando historial de vacunas...")
-
+            if m == 1:
+                print("Mostrando mascotas activas...")
+            elif m == 2:
+                print("Agregar nueva mascota...")
+            elif m == 3:
+                j = int(input("Mostrar Razas // presione(1)\nAgregar raza // presione(2)\nEliminar raza // presione(3)\n--> "))
+                if j == 1:
+                    raza = Razas.mostrarRaza()
+                elif j == 2:
+                    raza = Razas(input("Que raza desea agregar --> "))
+                    raza.añadirRaza()
+                elif j == 3:
+                    eliminar_raza = Razas(input("Que raza desea eliminar --> "))
+                    eliminar_raza.eliminarRaza()
+            elif m == 4:
+                print("Mostrando historial de vacunas...")
+        elif n == 3:
+            m = int(input(
+            "Tratamientos Disponibles // presione(1)\n"
+            "Agenda de Tratamientos // presione(2)\n"
+        ))
             if m == 1:
                 print("Mostrando tratamientos disponibles...")
             elif m == 2:
